655f4dc09808a47b919bd5faf83f66c8cb5b0099
DHANUSH G <dhanushsankar4567@gmail.com>
2026-05-11 21:34:24 +0530

    Update BestTimeToBuyAndSellStock.java

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,17 +1,3 @@
-// LeetCode #121 - Best Time to Buy and Sell Stock
-// NeetCode: Sliding Window
-// Difficulty: Easy
-//
-// Problem:
-// You are given an array prices where prices[i] is the price of a stock on day i.
-// Return the maximum profit you can achieve from one buy and one sell.
-// If no profit is possible, return 0.
-//
-// Example:
-// Input: prices = [7,1,5,3,6,4]
-// Output: 5 (Buy on day 2 at price 1, sell on day 5 at price 6)
-//
-// Approach: Two Pointers (min tracker) - O(n) Time | O(1) Space
 
 public class BestTimeToBuyAndSellStock {
 
